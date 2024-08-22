08c167d0f805ab4a56c985437ab0f7e4317a7520
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-08-22 10:09:08 -0400

    Added setSelectionOption and handle, as well as showValueForm state

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [showValueForm, setShowFormValue] = useState(false);
+  const [selectOption, setSelectionOption] = useState("");
+  const [newValue, setNewValueInput] = useState([]);
+
+  const toggleNewValueForm = () => {
+    setShowFormValue(!showValueForm);
+  };
+  const handleSelectOption = (e) => {
+    setSelectionOption(e.target.value);
+  }
+  // const handleTextChange = () => {
+
+  // };
+
   return (
     <div>
       <form>
+        <label htmlFor="" type="text"> Values </label>
         <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+        <select id="operation" name="operation" onChange={handleSelectOption}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
